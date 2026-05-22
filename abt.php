f3cfba80c7a74fd37218a466ebff4e455d356291
Phoebe Taylor <phoebe.t@wustl.edu>
2026-05-22 15:43:53 -0500

    Initial commit

---
+++ abt.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Page</title>
+    <link rel="stylesheet" href="abt.css">
+</head>
+<body>
+    <header>
+        <a href="personal.php">Back</a>
+        <h1>About</h1>
+    </header>
+
+    <section class="info">
+        <p>Hi everyone! My name is Phoebe Taylor. I am a sophomore at Washington University in St.Louis (WashU) where I am majoring in Computer Science with a double minor
+            in Chinese and Human Computer Interation. I love to read, write, draw, and basically learn about all things art related. My favorite coding languages to work with are HTML, CSS, and a bit of react.
+            I'm really excited to work with everyone this summer!
+        </p>
+    </section>
+</body>
+</html>
---
+++ include/connect.php
@@ -0,0 +1,6 @@
+
+<?php
+    DEFINE('DB_HOSTNAME', 'localhost');
+    DEFINE('DB_DATABASE', 'personal_site');
+    DEFINE('DB_USERNAME', 'root');
+    DEFINE('DB_PASSWORD', 'root');
