aef46e8c2fe5ce9189ed699f54e72e78c6ac1437
Phoebe Taylor <phoebe.t@wustl.edu>
2026-05-26 13:57:42 -0500

    adding text animations

--- abt.php
+++ abt.php
@@ -22,22 +22,22 @@
         <section class="images">
             <a href="https://www.goodreads.com/">
                 <h2>Reading</h2>
-                <img src="https://hips.hearstapps.com/hmg-prod/images/dahlia-1508785047.jpg?crop=1.00xw:0.669xh;0,0.0136xh" alt="" class="main-img">
+                <img src="images/blue-book.png" alt="book" class="main-img">
                 <img src="https://creativepaint.com/cdn/shop/products/2076-60-dogsear_3d0bc9a5-68b3-4396-96d6-570172381a37_1600x.png?v=1617304306" alt="" class="color-image">
             </a>
             <a href="https://youtu.be/-BR-B0kTmSI?si=aWdvVY6L3wD-3LyT">
                 <h2>Dancing</h2>
-                <img src="https://i.pinimg.com/564x/b5/9d/9e/b59d9e9449cb29c2a24fc41643405ab1.jpg" alt="" class="main-img">
+                <img src="images/dance.jpeg" alt="dance" class="main-img">
                 <img src="https://preview.colorkit.co/color/ffb6c1.png?size=vertical-wallpaper&static=true" alt="" class="color-image">
             </a>
-            <a href="https://youtu.be/KqMOKCRgvD8?si=44ipVbG8OS8TeyN2">
+            <a href="https://www.youtube.com/watch?v=KqMOKCRgvD8">
                 <h2>Guitar</h2>
-                <img src="https://www.verdissimo.com/wp-content/uploads/2023/08/Verdissimo_flores_bonitas_CONT_09.jpg" alt="" class="main-img">
+                <img src="images/guitar.png" alt="" class="main-img">
                 <img src="https://www.colorpalettestore.com/cdn/shop/products/F8E3E8_1024x.png?v=1750858375" alt="" class="color-image">
             </a>
             <a href="https://grandbaby-cakes.com/sweet-potato-cinnamon-rolls/">
                 <h2>Cooking/Baking</h2>
-                <img src="https://www.dutchgrown.co.uk/cdn/shop/articles/shutterstock_2306718573_732a0a25-2052-41cc-82f8-389b9545384e_789x_crop_center.jpg?v=1738857655" alt="" class="main-img">
+                <img src="images/cake.png" alt="" class="main-img">
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5waZ67StVOOJdCE1p-orC9lc0HtTWUMo73Q&s" alt="" class="color-image">
             </a>
         </section>
