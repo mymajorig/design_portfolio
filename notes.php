0c91fea64e0119144b5167f9ee4f0188d05adaa4
Phoebe Taylor <phoebe.t@wustl.edu>
2026-06-17 09:33:35 -0500

    pushing changes to transfer files to github repo

--- notes.php
+++ notes.php
@@ -10,37 +10,22 @@ if(isset($_POST['saveComment'])){
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Form Notes</title>
-</head>
-<body>
+<form action='' method='post'>
     <h2>Leave a comment</h2>
-    <form action="">
-        <label for="name">Name</label>
-        <input type='text' name = 'name-content' id='name' value=''>
-        <br>
-        <br>
-        <label for="comment">Comment</label>
-        <input type='text' name = 'comments' id='comments' value=''>
-        <br>
-        <br>
-        <input type='submit' value='Save and Submit'>
+    Name: <input type='text' name='name' />
+    <br/><br/>
 
-    </form>
+    Comment: <textarea name='comment' style='width:400px; height:100px;'></textarea>
+    <br/><br/>
+    <input type='submit' name='saveComment' />
 
-</body>
-</html>
+</form>
 
 <?php
-
 function saveThisComment($name, $comment){
     dbQuery("
         INSERT INTO test(name,comment)
         VALUES('$name', '$comment')
-    ");
+    "); 
     }
 
